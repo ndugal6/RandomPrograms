5c2d79ea84cd69ef2af8a7a87ba938a8364c7c00
ndugal6 <18618975+ndugal6@users.noreply.github.com>
2020-04-01 14:19:58 -0500

    aye it works! Now I can send & read this on kindle without weird format :)

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -10,29 +10,26 @@ def main():
     input1 = PdfFileReader(open("/Users/odin/Downloads/What-is-Life-By-Erwin-Schrodinger.pdf", "rb"))
     print("document1.pdf has %d pages." % input1.getNumPages())
     page1 = input1.getPage(0)
-    page1_copy = copy(page1)
-
-    print(f'size of pdf pages: {page1.mediaBox.getUpperRight_x()} by {page1.mediaBox.getUpperRight_y()}')
     print(f'raw size of pdf pages: {page1.mediaBox}')
+
     width_trim_1 = 75
     width_trim_2 = 3
+    top_trim = 52
+    bottom_trim = 60
+    first_half_split = RectangleObject([width_trim_1, bottom_trim, 306 - width_trim_2, 792 - top_trim])
+    second_half_split = RectangleObject([306 + width_trim_2, bottom_trim, 612 - width_trim_1, 792 - top_trim])
 
-    # first_half_split = RectangleObject([60,60,303, 740])
+    for page_num in range(0,input1.getNumPages()):
+        p = input1.getPage(page_num)
+        p_copy = copy(p)
+        p.mediaBox = first_half_split
+        p_copy.mediaBox = second_half_split
+        output.addPage(p)
+        output.addPage(p_copy)
 
-    first_half_split = RectangleObject([width_trim_1,60,306-width_trim_2, 740])
-
-    second_half_split = RectangleObject([306+width_trim_2, 60, 612-width_trim_1, 740])
-    page1.mediaBox = first_half_split
-    page1_copy.mediaBox = second_half_split
-
-
-    output.addPage(page1)
-    output.addPage(page1_copy)
-
-    output_stream = open("PyPDF2-output.pdf", "wb")
+    output_stream = open("pdf_to_text_output.pdf", "wb")
     output.write(output_stream)
     output_stream.close()
-
 
 if __name__ == '__main__':
     main()
